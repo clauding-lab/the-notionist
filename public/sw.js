2bc5ca8e1612ba7af450609d1615afea63c6781d
Your Name <you@example.com>
2026-03-22 23:57:31 +0600

    Add proper PWA icons (SVG + PNG 192/512)

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "the-notionist-v1";
-const ASSETS = ["./", "./manifest.json"];
+const ASSETS = ["./", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
